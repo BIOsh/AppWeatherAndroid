f992712ad891adcb4097dd2b7d589c2b1c68a239
BIOsh <kompavel95>
2019-02-05 14:50:45 +1000

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Common/Common.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.Common;
+
+import android.location.Location;
+
+public class Common {
+
+    public static final String APP_ID = "11e70dfece2dae0b8a5a43422c26d348";
+    public static Location current_location = null;
+
+}
